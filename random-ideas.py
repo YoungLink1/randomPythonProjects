a7783474e2cf02b341fbaffe8145eb471d42a219
Young Link <153998400+YoungLink1@users.noreply.github.com>
2026-07-23 17:36:30 -0600

    Merged local files, added a new test.
    - Added classes: "Polygon" and "DragPolygon"
    - Added test 8.

--- random-ideas.py
+++ random-ideas.py
@@ -11,6 +11,18 @@ def clear_console():
     else:
         os.system('clear')
 
+# Reloads the Program.
+def reload_program(current_filename=os.path.basename(__file__)):
+    """Runs this program again. If ran in the middle of a program, the program that ran this function will continue after the ran function is over.
+    
+    Args:
+        current_filename: The filename of this Python file.
+
+    Returns:
+        Runs the called Python file and then resumes the Python file that called it, once done.
+    """
+    os.system("python " + current_filename)
+
 # Converts between a Boolean and Binary output for simplicity.
 def state_convert(var):
     """Converts a Binary (1 or 0) / Boolean (True or False) to the opposite form.
@@ -92,9 +104,10 @@ except ImportError:
             clear_console()
 # End of Library / Function Importing.
 
-# Start of Global Pygame Related Functions:
+# Start of Global Pygame Related Functions / Classes:
 def poll_mouse(value=int()):
-        """Uses the pygame.mouse.get_pressed function and gives an easier to read and use output of where the mouse is or what mouse buttons are pressed. Supports 3 & 5 button mice.
+        """Gives an easy to read and use output of various mouse actions.
+        Supports 3 & 5 button mice.
         Outputs as a boolean.
 
         MOUSE POSITION:
@@ -104,7 +117,7 @@ def poll_mouse(value=int()):
         -4 = Mouse X [DESKTOP]
         -5 = Mouse Y [DESKTOP]
         -6 = Combined DESKTOP Mouse X & Y
-
+        \n
         BUTTONS:
         1 = Left Mouse Button
         2 = Middle Mouse Button (clicked, no scroll)
@@ -155,15 +168,68 @@ def poll_mouse(value=int()):
                 for event in pygame.event.get():
                     if event.type == MOUSEBUTTONDOWN:
                         if event.button == 4:
-                            return int(1)
+                            return 1
                         elif event.button == 5:
-                            return int(-1)
-                        else:
-                            return int(0)
+                            return -1
+                return 0
             case _:
                 return('Invalid Poll Action in FUNC: "poll_mouse()"')
 
-# End of Global Pygame Related Functions.
+class Polygon:
+        """Class meant for creating objects within pygame off of a list of points.
+        -------------------------------------------------------
+        def __init__(self, points, screen_size=(480, 360)):
+            self.points = points
+
+            self.surface = pygame.Surface(screen_size, pygame.SRCALPHA)
+            pygame.draw.polygon(self.surface, (255, 255, 255), self.points)
+            self.mask = pygame.mask.from_surface(self.surface)
+
+        def is_hovered(self, position):
+            return bool(self.mask.get_at(position))
+            
+        def is_clicked(self, position):
+            return self.is_hovered(position) & poll_mouse(1)"""
+
+        def __init__(self, points, screen_size=(480, 360)):
+            self.points = points
+
+            # 1. Bake the surface & mask it ONCE on creation.
+            self.surface = pygame.Surface(screen_size, pygame.SRCALPHA)
+            pygame.draw.polygon(self.surface, (255, 255, 255), self.points)
+            self.mask = pygame.mask.from_surface(self.surface)
+
+        # When called, checks on the mask if the position has a pixel or not.
+        def is_hovered(self, position):
+            """Check for collision using the pre-baked mask, to see if there's overlap with the given position."""
+            return bool(self.mask.get_at(position))
+
+        def is_clicked(self, position):
+            """Check if object is hovered and then clicked. Uses 'is_hovered()' to check collision."""
+            return self.is_hovered(position) & poll_mouse(1)
+
+class DragPolygon(Polygon):
+        "Subclass of Polygon meant for dragable objects."
+        def __init__(self, points, neededHold):
+            self.held, self.offset_x, self.offset_y = 0, 0, 0
+            self.draggable = True
+            prior_x = 0
+            prior_y = 0
+            current_x = poll_mouse(-1)
+            current_y = poll_mouse(-2)
+            
+            while self.draggable:
+                prior_x = poll_mouse
+                prior_y = 0
+
+                if self.is_clicked((current_x, current_y)) & self.held > neededHold:
+                    return "dragging"
+                elif self.is_clicked((current_x, current_y)):
+                    self.held += 1
+                else:
+                    self.held = 0
+
+# End of Global Pygame Related Functions / Classes.
 
 PS4_button_ref = ["Cross Button", "Circle Button", "Square Button", "Triangle Button", "Share Button", "PS Button", "Options Button", "Left Stick", "Right Stick", "Left Bumper", "Right Bumper", "D-Pad Up", "D-Pad Down", "D-Pad Left", "D-Pad Right", "Touch Pad"]
 PS4_axis_ref = ["LS X-Axis", "LS Y-Axis", "RS X-Axis", "RS Y-Axis", "Left Trigger", "Right Trigger"]
@@ -173,6 +239,8 @@ clear_console()
 
 if DEBUG_testItem == 0:
     os.system("%USERPROFILE%/AppData/Roaming/Python/Python310/site-packages/pygame/docs/generated/ref/draw.html")
+    os.system("%USERPROFILE%/AppData/Local/Programs/Python/Python310/Lib/site-packages/pygame/docs/generated/ref/draw.html")
+    reload_program()
 
 elif DEBUG_testItem == 1:
     #try to make an interactive UI/GUI
@@ -283,14 +351,17 @@ elif DEBUG_testItem == 5:
                 running = False
         
 
-        poly_LMB = [(45, 90), (45, 0), (33.75, 2.25), (24.75, 6.75), (15.75, 13.5), (9, 24.75), (3.6, 38.25), (0.9, 58.5), (0, 90)]
-        poly_RMB = [(45, 90), (45, 0), (56.25, 2.25), (65.25, 6.75), (74.25, 13.5), (81, 24.75), (86.4, 38.25), (89.1, 58.5), (90, 90)]
-        poly_MMB = [(38, 15), (52, 15), (52, 55), (38, 55)]
+        poly_LMB = [(44.5, 89), (44.5, 0), (33.375, 2.225), (24.475, 6.675), (15.575, 13.35), (8.9, 24.475), (3.56, 37.825), (0.89, 57.85), (0, 89)]
+        poly_RMB = [(44.5, 89), (44.5, 0), (55.625, 2.225), (64.525, 6.675), (73.425, 13.35), (80.1, 24.475), (85.44, 37.825), (88.11, 57.85), (89, 89)]
+        poly_MMB = [(37, 15), (52, 15), (52, 55), (37, 55)]
         poly_SCWU = poly_MMB[0:1] + [(52, 35), (38, 35)]
         poly_SCWD = [(52, 35), (38, 35)] + poly_MMB[2:3]
 
 
         screen.fill((30, 30, 30))
+
+        scroll = poll_mouse(6)
+        #print(f"Pos: {poll_mouse(-3)}; Scroll: {scroll}")
 
 
         if poll_mouse(1):
@@ -312,10 +383,10 @@ elif DEBUG_testItem == 5:
         else:
             pygame.draw.polygon(screen, (50, 50, 50), poly_MMB, 0)
 
-        if poll_mouse(6) > 0:
+        if scroll > 0:
             pygame.draw.polygon(screen, (200, 50, 80), poly_SCWU, 0)
             #input("scroll is above 0")
-        elif poll_mouse(6) < 0:
+        elif scroll < 0:
             pygame.draw.polygon(screen, (200, 50, 80), poly_SCWD, 0)
             #input("scroll is below 0")
         #elif poll_mouse(6) == 0:
@@ -324,7 +395,7 @@ elif DEBUG_testItem == 5:
         pygame.draw.polygon(screen, (80, 80, 80), poly_MMB, 2)
 
 
-        print(poll_mouse(6))
+        #print(poll_mouse(6))
 
 
         pygame.display.flip()
@@ -369,6 +440,7 @@ elif DEBUG_testItem == 6:
 
             # Handle Button Inputs
             if event.type == pygame.JOYBUTTONDOWN:
+                input(event)
                 print(f"[Joystick '{event.instance_id}'] {PS4_button_ref[event.button]} pressed.") # PS4 Support
 
             if event.type == pygame.JOYBUTTONUP:
@@ -387,3 +459,69 @@ elif DEBUG_testItem == 6:
 
     pygame.joystick.quit()
     pygame.quit()
+
+elif DEBUG_testItem == 7:
+    pygame.init()
+    screen = pygame.display.set_mode((480, 360))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        clear_console()
+        if pygame.key.get_pressed()[pygame.K_a]:
+            print("aajgjhagjsgagsjh")
+        else:
+            print("ahh")
+
+        pygame.display.flip()
+        clock.tick(30)
+
+elif DEBUG_testItem == 8:
+    pygame.init()
+    pygame.display.set_caption("Collision Testing")
+    screen = pygame.display.set_mode((480, 360))
+    clock = pygame.time.Clock()
+    running = True
+
+    poly_LMB = [(44.5, 89), (44.5, 0), (33.375, 2.225), (24.475, 6.675), (15.575, 13.35), (8.9, 24.475), (3.56, 37.825), (0.89, 57.85), (0, 89)]
+    btn_LMB = DragPolygon(poly_LMB, 5)
+    LMB_held = 0
+
+    while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((30, 30, 30))
+
+        mouse_pos = poll_mouse(-3)
+        clear_console()
+        if btn_LMB.is_clicked(mouse_pos):
+            LMB_held += 1
+            if LMB_held >= 10:
+                pygame.draw.polygon(screen, (90, 90, 105), poly_LMB, 0)
+                print(f"held click {LMB_held}")
+            else:
+                pygame.draw.polygon(screen, (90, 90, 90), poly_LMB, 0)
+                print(f"clicked {LMB_held}")
+        elif btn_LMB.is_hovered(mouse_pos):
+            pygame.draw.polygon(screen, (70, 70, 70), poly_LMB, 0)
+            LMB_held = 0
+            print(f"hovered {LMB_held}")
+        else:
+            pygame.draw.polygon(screen, (50, 50, 50), poly_LMB, 0)
+            LMB_held = 0
+            print(f"none {LMB_held}")
+
+        pygame.draw.polygon(screen, (160, 160, 160), poly_LMB, 2)
+
+        pygame.display.flip()
+        clock.tick(30) # limits FPS to 30
